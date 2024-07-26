7ea4d4e088e0054605f07564650d3aa01903c89f
jamieyello <jamieyello@proton.me>
2024-07-26 01:30:34 -0400

    Add tracked dictionary and tracked list

---
+++ NoDb/DiffTracking/ChangeLogIndex.cs
@@ -0,0 +1,21 @@
+namespace NoDb.DiffTracking;
+
+public enum ChangeActionType {
+    Set,
+    Add,
+    Insert,
+    Remove,
+    Clear
+}
+
+public readonly struct ChangeLogIndex<TKey, TValue> {
+    public readonly TKey? Index;
+    public readonly ChangeActionType Action;
+    public readonly TValue? Value;
+
+    public ChangeLogIndex(TKey? index, ChangeActionType action, TValue? value = default) {
+        Index = index;
+        Action = action;
+        Value = value;
+    }
+}
